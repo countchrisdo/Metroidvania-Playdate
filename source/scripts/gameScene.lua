0a32f16b9a532ae02ea0ce26f5d20f7b9dbe34cc
countchrisdo <crburwell@yahoo.com>
2025-03-18 13:52:19 -0400

    Remove unused builds and fixed zIndex issue

--- source/scripts/gameScene.lua
+++ source/scripts/gameScene.lua
@@ -13,8 +13,14 @@ function GameScene:init()
 end
 
 function GameScene:goToLevel(level_name)
+
     gfx.sprite.removeAll()
     print("GameScene:goToLevel("..level_name..")")
+
+    -- local layerstb = ldtk.get_layers(level_name)
+    -- print("printing layers:")
+    -- printTable(layerstb)
+
     for layer_name, layer in pairs(ldtk.get_layers(level_name)) do
         if layer.tiles then
             print("Creating tilemap for layer: "..layer_name)
@@ -27,19 +33,28 @@ function GameScene:goToLevel(level_name)
             layerSprite:setTilemap(tilemap)
             layerSprite:setCenter(0, 0)
             layerSprite:moveTo(0, 0)
-            if layer.zindex then
-                layerSprite:setZIndex(layer.zindex)
-            else
-                layerSprite:setZIndex(0) -- Default to 0 if zindex is nil
-            end
+            layerSprite:setZIndex(layer.zIndex)
             layerSprite:add()
 
-            -- Getting every empty tile ie everything not "Solid". Solid is an enum we created in LDtk
+            -- Getting every empty tile ie. everything not Solid. Solid is an enum we created in LDtk
             local emptyTiles = ldtk.get_empty_tileIDs(level_name, "Solid", layer_name)
             if emptyTiles then
                 gfx.sprite.addWallSprites(tilemap, emptyTiles)
             end
 
+        end
+    end
+end
+
+function printTable(t, indent)
+    indent = indent or 0
+    for k, v in pairs(t) do
+        local formatting = string.rep("  ", indent) .. k .. ": "
+        if type(v) == "table" then
+            print(formatting)
+            printTable(v, indent + 1)
+        else
+            print(formatting .. tostring(v))
         end
     end
 end
