7c565146b38d920c4d66de47e1ee145d70ea9f47
countchrisdo <crburwell@yahoo.com>
2025-03-25 19:49:18 -0400

    Enhance player movement mechanics with jump, drag, and collision detection improvements

--- source/scripts/player.lua
+++ source/scripts/player.lua
@@ -30,9 +30,14 @@ function Player:init(x,y)
     self.yVelocity = 0
     self.gravity = 1.0
     self.maxSpeed = 2.0
+    self.jumpVelocity = -6
+    self.drag = 0.1
+    self.minimumAirSpeed = 0.5
 
     -- Player State
     self.touchingGround = false
+    self.touchingCeiling = false
+    self.touchingWall = false
 end
 
 -- Overwrite collisionResponse method
@@ -60,6 +65,8 @@ function Player:handleState()
             self:changeToIdleState()
         end
         self:applyGravity()
+        self:applyDrag(self.drag)
+        self:handleAirInput()
 
     end
 end
@@ -68,14 +75,25 @@ function Player:handleMovementAndCollisions()
     -- moves player by the x and y velocity and gets collisions arr
     local _, _, collisions, length = self:moveWithCollisions(self.x + self.xVelocity, self.y + self.yVelocity)
     -- collisions is an array of all the collisions that happened
+    -- setting default values
     self.touchingGround = false
-    -- setting touchingGround to false by default and checking if player is touching the ground
+    self.touchingCeiling = false
+    self.touchingWall = false
+    
     for i=1, length do
         local collision = collisions[i]
         if collision.normal.y == -1 then
         -- a normal is a vector that is perpendicular to the surface player is colliding with
         -- if the collision normal is -1, it means the player is touching the ground.
             self.touchingGround = true
+        elseif collision.normal.y == 1 then
+            self.touchingCeiling = true
+        end
+
+        -- if you're touching a wall, the x collision normal will be either 1 or -1 because collision normal is a vector that is perpendicular to the surface player is colliding with. So we can just check if the x collision normal is not 0. 
+        if collision.normal.x ~= 0 then
+            self.touchingWall = true
+            self.xVelocity = 0
         end
     end
     -- print("Touching Ground: " .. tostring(self.touchingGround))
@@ -90,14 +108,24 @@ end
 
 -- Input Helper Functions
 function Player:handleGroundInput()
-    -- using buttonIsPressed instead of buttonJustPressed because it's like a cheap way to add buffering to the inputs by changing states.
-    if pd.buttonIsPressed(pd.kButtonLeft) then
+    -- using buttonIsPressed instead of buttonJustPressed because it's like a cheap way to add buffering to the inputs by changing states. Using buttonJustPressed for jumping because jumping is a one time action.
+    if pd.buttonJustPressed(pd.kButtonA) then
+        self:changeToJumpState()
+    elseif pd.buttonIsPressed(pd.kButtonLeft) then
         self:changeToRunState("left")
     elseif pd.buttonIsPressed(pd.kButtonRight) then
         self:changeToRunState("right")
     else
         self:changeToIdleState()
     end
+end
+
+function Player:handleAirInput()
+    if pd.buttonIsPressed(pd.kButtonLeft) then
+        self.xVelocity = -self.maxSpeed
+    elseif pd.buttonIsPressed(pd.kButtonRight) then
+        self.xVelocity = self.maxSpeed
+    end  
 end
 
 -- State transitions
@@ -117,12 +145,29 @@ function Player:changeToRunState(direction)
     self:changeState("run")
 end
 
+function Player:changeToJumpState()
+    self.yVelocity = self.jumpVelocity
+    self:changeState("jump")
+end
+
 -- Physics Helper Functions
 function Player:applyGravity()
     self.yVelocity += self.gravity
     -- print("Applying Gravity")
     -- print("Y Velocity: " .. self.yVelocity)
-    if self.touchingGround then
+    if self.touchingGround or self.touchingCeiling then
         self.yVelocity = 0
+    end
+end
+
+function Player:applyDrag(amount)
+    if self.xVelocity > 0 then
+        self.xVelocity -= amount
+    elseif self.xVelocity < 0 then
+        self.xVelocity += amount
+    end
+
+    if math.abs(self.xVelocity) < self.minimumAirSpeed or self.touchingWall then
+        self.xVelocity = 0
     end
 end
